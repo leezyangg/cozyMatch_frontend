bbeea66ac3fb64801e2495dd11261a94b644b285
leezyangg <ziyang8254@gmail.com>
2023-05-19 11:38:14 +0800

    css modification

--- src/components/LandlordLayout.jsx
+++ src/components/LandlordLayout.jsx
@@ -22,19 +22,22 @@ const logout = (ev) => {
 }
 
 
-export default function UserLayout() {
+export default function LandlordLayout() {
     return (
         <>
-            <div className="navbar">
-                <img src='/src/assets/cozy_match_icon.png' alt="cozyMatch_Icon" className="navbar--icon" />
-                <h3 className="navbar--projectName"><span className="font--bold">Cozy</span>Match</h3>
-                <div className="navbar--navlink">
+            <div className="flex items-center h-16 text-black mb-8 shadow-lg py-10 px-5">
+                <img src='/src/assets/cozy_match_icon.png' alt="cozyMatch_Icon" className="mr-1 h-12" />
+                <h3 className="h-6 font-hairline"><span className="font-bold">Cozy</span>Match</h3>
+                <div className="ml-12">
                     {navigation.map((item) => (
                         <NavLink
                             key={item.name}
                             to={item.to}
                             className={({ isActive }) => (
-                                isActive ? 'active' : 'pending'
+                                isActive
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'rounded-md px-3 py-2 text-sm font-medium'
                             )}
                         >
                             {item.name}
@@ -42,26 +45,26 @@ export default function UserLayout() {
                     ))}
                 </div>
 
-                <Menu as="div" className="navbar--menu">
+                <Menu as="div" className="relative ml-auto flex items-center text-sm">
                     <div className="navbar--user">
-                        <Menu.Button className="navbar--menu--button">
-                            <img className="navbar--profile--pic" src={user.imageUrl} alt="" />
+                        <Menu.Button className="flex text-sm leading-5 items-center max-w-xs rounded-full">
+                            <img className="w-12 h-12 rounded-full" src={user.imageUrl} alt="" />
                         </Menu.Button>
                     </div>
-                    <Menu.Items className="navbar--profile--pic--rounded">
-                        <Menu.Item>
-                            <a
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Item>
+                              <a
                                 href="#"
                                 onClick={(ev) => logout(ev)}
-                                className={'profile--picture--block'}
-                            >
+                                className={'block px-4 py-2 text-sm text-gray-700'}
+                              >
                                 Sign Out
-                            </a>
+                              </a>
                         </Menu.Item>
                     </Menu.Items>
-                    <div className="user--profile--info">
+                    <div className="flex flex-col pl-2">
                         <div>{user.name}</div>
-                        <div className="user--profile--type">{user.userType}</div>
+                        <div className="text-xs text-sky-600">{user.userType}</div>
                     </div>
                 </Menu>
             </div>
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,63 +1,63 @@
 export default function LoginPage() {
     return (
         <div>
-            <div className="login--header">
-                <div className="login--word">
+            <div className="flex pl-4 pr-4 pt-12 pb-12 justify-center items-center min-h-full">
+                <div>
                     <div>
                         <img
-                            className="h-5"
+                            className="h-48 m-auto"
                             src="\src\assets\cozy_match_icon.png"
                             alt="CozyMatch"
                         />
                     </div>
-                    <h2 className="login--title">
-                Sign in to your account
-            </h2>
+                    <h2 className="mt-6 text-gray-900 text-3xl leading-9 font-bold tracking-tight text-center">
+                        Sign in to your account
+                    </h2>
 
-            <form className="login--form" action="#" method="POST">
-                <input type="hidden" name="remember" defaultValue="true" />
-                <div className="login--form--div">
-                    <div className="login--input--field">
-                        <label htmlFor="email-address" className="sr-only">
-                            Email address
-                        </label>
-                        <input
-                            id="email-address"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            className="login--form--email"
-                            placeholder="Email address"
-                        />
-                    </div>
-                    <div className="login--input--field">
-                        <label htmlFor="password" className="sr-only">
-                            Password
-                        </label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="current-password"
-                            required
-                            className="login--form--password"
-                            placeholder="Password"
-                        />
-                    </div>
-                </div>
+                    <form className="mt-8 space-y-6" action="#" method="POST">
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="-space-y-px rounded-md shadow-sm">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
 
-                <div>
-                    <button
-                        type="submit"
-                        className="login--form--button"
-                    >
-                        <span className="login--button--text">
-                        </span>
-                        Login
-                    </button>
-                </div>
-            </form>
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                </span>
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
