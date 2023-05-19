4cea87fd8c0e5ec5456a66aa88e47d5b85d3d29f
leezyangg <ziyang8254@gmail.com>
2023-05-19 13:45:08 +0800

    small changes

--- src/views/RoomListing.jsx
+++ src/views/RoomListing.jsx
@@ -1,10 +1,11 @@
 import RoomInfoCard from "../components/RoomInfoCard";
-import listedroom from "../assets/listedRoom/data"
+import listedroom from "../assets/listedRoom/data";
+import { HiLocationMarker } from 'react-icons/hi';
 
 export default function RoomListing() {
     const listedRoomCard = listedroom.map(listedRoom => {
         return (
-            <RoomInfoCard 
+            <RoomInfoCard
                 key={listedRoom.id}
                 {...listedRoom}
             />
@@ -12,8 +13,15 @@ export default function RoomListing() {
     })
 
     return (
-        <div className="space-y-7">
-            {listedRoomCard}
+        <div>
+            <h2 className="mx-20 font-bold text-2xl">Room Listing</h2>
+            <div className="flex mx-20 mt-3 mb-5 items-center">
+                <HiLocationMarker size={20} />
+                <h3>Taman Serdang Java, Sri Kembangan, Selangor</h3>
+            </div>
+            <div className="space-y-7">
+                {listedRoomCard}
+            </div>
         </div>
     )
 }
