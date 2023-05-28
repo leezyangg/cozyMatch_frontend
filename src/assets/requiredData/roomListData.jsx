99d3b87073ed7fc3d6565450ffa6ca1c5b4bea05
LongYehMin <longyehmin@gmail.com>
2023-05-28 18:56:11 +0800

    added room price marker on map

--- src/assets/requiredData/roomListData.jsx
+++ src/assets/requiredData/roomListData.jsx
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         img: "src/assets/room.jpg",
-        address: "No 32, Jalan Bukit Serdang, Taman Bukit Serdang, 43400 Serdang Selangor",
+        address: "Jalan Bukit Serdang, Taman Bukit Serdang, 43400 Serdang Selangor",
         type: "Condominium",
         rentalFee: 1300,
         bedroom: 5,
@@ -13,8 +13,7 @@ export default [
     {
         id: 2,
         img: "src/assets/room.jpg",
-        address: "No 32, Jalan Bukit Serdang, Taman Bukit Serdang, 43400 Serdang Selangor",
-        type: "Condominium",
+        address: "Jalan BS 9/5, Taman Bukit Serdang Seksyen 9",
         rentalFee: 1500,
         bedroom: 3,
         bathroom: 1,
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,12 +3,35 @@ import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import { Text, Fill } from 'ol/style';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { fromLonLat } from 'ol/proj';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
 import { Icon, Style } from 'ol/style';
+import listedroom from "../assets/requiredData/roomListData";
+
+// Function to convert address to coordinates using OpenStreetMap Nominatim API
+async function geocodeAddress(address) {
+  const encodedAddress = encodeURIComponent(address);
+  const url = `https://nominatim.openstreetmap.org/search?q=${encodedAddress}&format=json`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (Array.isArray(data) && data.length > 0) {
+      const { lat, lon } = data[0];
+      return [parseFloat(lon), parseFloat(lat)]; // Return coordinates as [longitude, latitude]
+    } else {
+      throw new Error('Unable to geocode the address.');
+    }
+  } catch (error) {
+    console.error('Error geocoding address:', error);
+    throw error;
+  }
+}
 
 export default function MapComponent() {
   const mapContainerRef = useRef(null);
@@ -28,7 +51,7 @@ export default function MapComponent() {
       }),
     });
 
-    // Create a vector layer and source for the marker
+    // Create a vector layer and source for the markers
     const vectorSource = new VectorSource();
     const vectorLayer = new VectorLayer({
       source: vectorSource,
@@ -38,32 +61,76 @@ export default function MapComponent() {
     // Get the current live location
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
+        async (position) => {
           const { latitude, longitude } = position.coords;
 
-          // Create a marker feature
-          const markerFeature = new Feature({
+          // Create a marker feature for the current location
+          const currentLocationFeature = new Feature({
             geometry: new Point(fromLonLat([longitude, latitude])),
           });
 
-          // Create a style for the marker
-          const markerStyle = new Style({
+          // Create a style for the current location marker
+          const currentLocationStyle = new Style({
             image: new Icon({
               anchor: [0.5, 1],
               src: '/src/assets/pinCurrent.png', // Replace with the correct path to your pinCurrent.png icon
             }),
           });
 
-          // Set the style for the marker feature
-          markerFeature.setStyle(markerStyle);
+          // Set the style for the current location marker feature
+          currentLocationFeature.setStyle(currentLocationStyle);
 
-          // Add the marker feature to the vector source
-          vectorSource.addFeature(markerFeature);
+          // Add the current location marker feature to the vector source
+          vectorSource.addFeature(currentLocationFeature);
 
-          // Center the map to the marker's location
+          // Loop through the room details and create marker features for each room
+          for (const room of listedroom) {
+            const { address, rentalFee } = room;
+
+            try {
+              // Convert address to coordinates
+              const coordinates = await geocodeAddress(address);
+
+              // Create a marker feature for the room
+              const roomFeature = new Feature({
+                geometry: new Point(fromLonLat(coordinates)),
+              });
+
+             // Create a style for the room marker
+                const roomStyle = new Style({
+                  image: new Icon({
+                    anchor: [0.5, 1],
+                    src: '/src/assets/pinRoom.png', // Replace with the correct path to your pinRoom.png icon
+                    scale: [0.3,0.2],
+                    imgSize: [20, 10],
+                    offset: [0, -25], // Move the image slightly upwards
+                  }),
+                  text: new Text({
+                    text: `RM${rentalFee}`, // Display the room price as text
+                    font: 'bold 12px Arial',
+                    textAlign: 'center',    // Set text alignment to center
+                    textBaseline: 'middle', // Set text baseline to middle
+                    fill: new Fill({        // Define fill color
+                      color: '#ffffff',     // Set the rentalFee text color
+                    }),
+                    offsetY: -20, // Move the text slightly upwards
+                  }),
+                });
+
+              // Set the style for the room marker feature
+              roomFeature.setStyle(roomStyle);
+
+              // Add the room marker feature to the vector source
+              vectorSource.addFeature(roomFeature);
+            } catch (error) {
+              console.error(`Error geocoding address "${address}":`, error);
+            }
+          }
+
+          // Center the map to the current location
           map.getView().setCenter(fromLonLat([longitude, latitude]));
 
-          // Zoom to the marker's location
+          // Zoom to the current location
           map.getView().setZoom(15);
         },
         (error) => {
