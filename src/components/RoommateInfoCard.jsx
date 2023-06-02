76ec7f94eaa11c8d47ad073a199cb7c4489f8747
Izzy29 <m.izrinsyafiq@gmail.com>
2023-06-03 02:23:50 +0800

    Changes in RIC and RM

--- src/components/RoommateInfoCard.jsx
+++ src/components/RoommateInfoCard.jsx
@@ -1,75 +1,113 @@
+import React from 'react';
 import { GrMapLocation } from 'react-icons/gr';
 import { BiMoneyWithdraw } from 'react-icons/bi';
-import PropTypes from 'prop-types';
 
-export default function RoommateInfoCard(props) {
-    let scoreStyle
-    if (props.matchScore >= 80) {
-        scoreStyle = 'bg-lime-400 flex flex-col items-center rounded-b-xl py-2';
-    } else if (props.matchScore >50) {
-        scoreStyle = 'bg-amber-300 flex flex-col items-center rounded-b-xl py-2';
-    } else {
-        scoreStyle = 'bg-amber-700 flex flex-col items-center rounded-b-xl py-2';	
+const user = {
+    smoking: false,
+    pet: true,
+    drink: false,
+    isEarly: true,
+};
+
+export default function RoommateInfoCard({ roommate }) {
+
+    let matchScore = 0;
+    let score = 0;
+
+    if (user.smoking === roommate.smoking) {
+        score += 1;
     }
+    if (user.pet === roommate.pet) {
+        score += 1;
+    }
+    if (user.drink === roommate.drink) {
+        score += 1;
+    }
+    if (user.isEarly === roommate.isEarly) {
+        score += 1;
+    }
+
+    if (score === 4) {
+        matchScore = 100;
+    } else if (score === 3) {
+        matchScore = 75;
+    } else if (score === 2) {
+        matchScore = 50;
+    } else if (score === 1) {
+        matchScore = 25;
+    } else {
+        matchScore = 0;
+    }
+
+    let scoreStyle
+    if (matchScore >= 80) {
+        scoreStyle = 'bg-lime-400 flex flex-col items-center rounded-b-xl py-2 w-full';
+    } else if (matchScore > 50) {
+        scoreStyle = 'bg-amber-300 flex flex-col items-center rounded-b-xl py-2 w-full';
+    } else {
+        scoreStyle = 'bg-amber-700 flex flex-col items-center rounded-b-xl py-2 w-full';
+    }
+
     return (
         <div>
             <div className="shadow-xl rounded-xl bg-transparent">
                 <div className="flex flex-col items-center space-y-5 bg-stone-100 py-5 rounded-t-xl">
                     <div className="flex flex-col items-center">
                         <img src="src\assets\react.svg" alt="renter face" className="mb-2" />
-                        <h4 className="font-semibold">{props.name}</h4>
-                        <h4 className="text-sm">{props.occupation}</h4>
+                        <h4 className="font-semibold">{roommate.name}</h4>
+                        <h4 className="text-sm">{roommate.occupation}</h4>
                     </div>
                     <div className="flex flex-col items-center">
                         <h4 className="font-semibold">Clean room</h4>
-                        <h4 className="text-sm">{props.cleanRoomFrequency} per week</h4>
+                        <h4 className="text-sm">{roommate.cleanRoomFrequency} per week</h4>
                     </div>
                     <div className="flex flex-col items-center space-y-1">
                         <h4 className="font-semibold">Preference</h4>
                         <div className="flex items-center space-x-4">
-                            <img src={props.smoking ? 'src/assets/images/yes-smoking-icon.png' : 'src/assets/images/no-smoking-icon.png'} alt="" className="h-5" />
-                            <img src={props.pet ? 'src/assets/images/pet-icon.png' : 'src/assets/images/no-pets-icon.png'} alt="" className="h-5" />
-                            <img src={props.drink ? 'src/assets/images/alcohol-icon.png' : 'src/assets/images/no-alcohol-icon.png'} alt="" className="h-5" />
-                            <img src={props.isEarly ? 'src/assets/images/early-bird-icon.png' : 'src/assets/images/night-owl-icon.png'} alt="" className="h-5" />
+                            <img
+                                src={roommate.smoking ? 'src/assets/images/yes-smoking-icon.png' : 'src/assets/images/no-smoking-icon.png'}
+                                alt=""
+                                className="h-5"
+                            />
+                            <img
+                                src={roommate.pet ? 'src/assets/images/pet-icon.png' : 'src/assets/images/no-pets-icon.png'}
+                                alt=""
+                                className="h-5"
+                            />
+                            <img
+                                src={roommate.drink ? 'src/assets/images/alcohol-icon.png' : 'src/assets/images/no-alcohol-icon.png'}
+                                alt=""
+                                className="h-5"
+                            />
+                            <img
+                                src={roommate.isEarly ? 'src/assets/images/early-bird-icon.png' : 'src/assets/images/night-owl-icon.png'}
+                                alt=""
+                                className="h-5"
+                            />
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-col items-center bg-stone-200	 py-5">
+                <div className="flex flex-col items-center rounded-b-xl bg-stone-200 flex-grow">
                     <h4 className="text-sm font-semibold">Extra Information</h4>
-                    <div className='flex items-center space-x-2'>
+                    <div className="flex items-center space-x-2">
                         <GrMapLocation />
-                        <h4 className='text-sm'>{props.distanceFromCollege} km from university</h4>
-
+                        <h4 className="text-sm">{roommate.distanceFromCollege} km from university</h4>
                     </div>
-                    <div className='flex items-center space-x-2'>
+                    <div className="flex items-center space-x-2">
                         <BiMoneyWithdraw />
-                        <h4 className='text-sm'>RM {props.renterFee} per month</h4>
+                        <h4 className="text-sm">RM {roommate.renterFee} per month</h4>
                     </div>
-                </div>
-
-                <div className={scoreStyle}>
-                    <h4 className='font-bold'>Match Score: {props.matchScore}%</h4>
+                    <div className={`mt-3 ${scoreStyle}`}>
+                        <h4 className='font-bold'>Match Score: {matchScore}%</h4>
+                    </div>
                 </div>
             </div>
-            <div className='flex flex-col items-center w-full mt-3'>
-                <button className='bg-amber-900 rounded-xl w-full text-white font-bold py-1'>Check Room</button>
+            <div className="flex flex-col items-center w-full mt-3">
+                <button className="bg-amber-900 rounded-xl w-full text-white font-bold py-1">Check Room</button>
             </div>
         </div>
-
-    )
+    );
 }
 
-RoommateInfoCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    occupation: PropTypes.string.isRequired,
-    cleanRoomFrequency:PropTypes.number.isRequired,
-    distanceFromCollege: PropTypes.number.isRequired,
-    renterFee: PropTypes.number.isRequired,
-    matchScore: PropTypes.number.isRequired,
-    availability: PropTypes.bool.isRequired,
-    smoking: PropTypes.bool.isRequired,
-    pet: PropTypes.bool.isRequired,
-    drink: PropTypes.bool.isRequired,
-    isEarly: PropTypes.bool.isRequired,
-  };
+
--- src/views/RoommateMatching.jsx
+++ src/views/RoommateMatching.jsx
@@ -1,21 +1,32 @@
-import RoommateInfoCard from "../components/RoommateInfoCard";
-import roommateData from "../assets/requiredData/roommateData";
+
+import React, { useEffect, useState } from 'react';
+import RoommateInfoCard from '../components/RoommateInfoCard';
 
 export default function RoommateMatching() {
-    const roommateInfoCard = roommateData.map(roommate => {
-        return (
-            <RoommateInfoCard
-                key={roommate.id}
-                {...roommate}
-            />
-        )
-    })
+    const [roommates, setRoommates] = useState(null);
+
+    useEffect(() => {
+        const fetchRoommates = async () => {
+            const response = await fetch('http://localhost:3000/api/roommates');
+            const json = await response.json();
+
+            if (response.ok) {
+                setRoommates(json);
+            }
+        };
+
+        fetchRoommates();
+    }, []);
+
 
     return (
-        <div>
+        <div className="roommate-matching">
             <div className="grid gap-10 grid-cols-5 grid-rows-2 px-10">
-                {roommateInfoCard}
+                {roommates &&
+                    roommates.map((roommate) => (
+                        <RoommateInfoCard key={roommate._id} roommate={roommate} />
+                    ))}
             </div>
         </div>
-    )
+    );
 }
